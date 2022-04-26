55b6a360cb42e7303389818237b98f3d53773091
Sheena Bonilla <sheena.myb@gmail.com>
2022-04-25 17:28:21 -0700

    commit tree ae8ea5212dd3b929763ad81ee5f56719da6ac722

---
+++ index.js
@@ -0,0 +1,31 @@
+function myEach(collection, callback) {
+    return collection;
+}
+
+function myMap(collection, callback) {
+
+}
+
+function myReduce() {
+
+}
+
+function myFind() {
+    
+}
+
+function myFilter() {
+
+}
+
+function mySize() {
+
+}
+
+function myFirst() {
+
+}
+
+function myLast() {
+    
+}
